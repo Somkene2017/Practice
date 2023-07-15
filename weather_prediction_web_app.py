828520ea0b7e8b85f7be336ce7bb7ccdccfe9848
Somkene Uzowulu <somkeneuzowulu@gmail.com>
2023-07-15 14:19:51 +0100

    sec

--- weather_prediction_web_app.py
+++ weather_prediction_web_app.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pickle
 import streamlit as st
+from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 
 loaded_model = pickle.load(open('model_pack.json', 'rb'))
