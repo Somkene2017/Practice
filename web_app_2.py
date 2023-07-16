844e4486b0b62e5b1d1ef43cb7c94c90f0b98775
Somkene Uzowulu <somkeneuzowulu@gmail.com>
2023-07-16 01:18:24 +0100

    final

--- web_app_2.py
+++ web_app_2.py
@@ -66,7 +66,7 @@ def main():
         worksheet_name = st.secrets.worksheet_name
         # Create a Google Sheet client
         dict = st.secrets.cred
-        gc = gspread.create_service_acoount(dict)
+        gc = gspread.create_service_account(dict)
 
         # Open the Google Sheet
         sh = gc.open_by_key(sheet_id)
