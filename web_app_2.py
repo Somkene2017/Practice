4840d9d44e6b0caa8ca15e45bcc3939486345e60
Somkene Uzowulu <somkeneuzowulu@gmail.com>
2023-07-16 19:59:16 +0100

    final

--- web_app_2.py
+++ web_app_2.py
@@ -66,17 +66,7 @@ def main():
         sheet_id = st.secrets.sheet_id
         worksheet_name = st.secrets.worksheet_name
         # Create a Google Sheet client
-        credentials_dict = {'type': st.secrets.type,
-                            'project_id': st.secrets.project_id,
-                            'private_key_id': st.secrets.private_key_id,
-                            'private_key': st.secrets.private_key,
-                            'client_email': st.secrets.client_email,
-                            'client_id': st.secrets.client_id,
-                            'auth_uri': st.secrets.auth_uri,
-                            'token_uri': st.secrets.token_uri,
-                            'auth_provider_x509_cert_url': st.secrets.auth_provider_x509_cert_url,
-                            'client_x509_cert_url': st.secrets.client_x509_cert_url,
-                            'universe_domain': st.secrets.universe_domain}
+        credentials_dict = st.secrets["cred"]
         gc = gspread.service_account_from_dict(credentials_dict)
         # Open the Google Sheet
         sh = gc.open_by_key(sheet_id)
